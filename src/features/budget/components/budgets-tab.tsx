2ca7704b9d48a849bf7efb1025dd2bc80bd9ed9c
Arif Amijoyo <arifamjy@gmail.com>
2024-05-12 20:57:39 +0700

    feat: add budget swipe action

--- src/features/budget/components/budgets-tab.tsx
+++ src/features/budget/components/budgets-tab.tsx
@@ -4,14 +4,74 @@ import { FlatList, TouchableOpacity, View } from 'react-native'
 import AddBudgetSheet from './add-budget-sheet'
 import { Link } from 'expo-router'
 import Container from '@/components/container'
+import { Iconify } from 'react-native-iconify'
+import CUSTOM_COLORS from '@/constants/colors'
+import { Swipeable } from 'react-native-gesture-handler'
+import uuid from 'react-native-uuid'
+
+const RightActions = ({ item }: { item: TBudget }) => {
+  const [, setBudgets] = useBudgetsAtom()
+
+  const handleCopyBudget = () => {
+    setBudgets((draft) => {
+      const budgetIndex = draft.findIndex((budget) => budget.id === item.id)
+      budgetIndex !== -1 &&
+        draft.push({
+          ...draft[budgetIndex],
+          name: draft[budgetIndex].name + ' (Copy)',
+          id: uuid.v4() as string,
+        })
+    })
+  }
+
+  const handleDeleteBudget = () => {
+    setBudgets((draft) => {
+      const budgetIndex = draft.findIndex((budget) => budget.id === item.id)
+      budgetIndex !== -1 && draft.splice(budgetIndex, 1)
+    })
+  }
+
+  return (
+    <View className="flex flex-row gap-6 px-3 items-center justify-center">
+      <TouchableOpacity
+        className="flex items-center justify-center"
+        onPress={handleCopyBudget}
+      >
+        <View className="mx-auto">
+          <Iconify icon="ph:copy" size={24} color={CUSTOM_COLORS.accent} />
+        </View>
+        <CustomText variant="accent">Copy</CustomText>
+      </TouchableOpacity>
+      <TouchableOpacity
+        className="flex items-center justify-center"
+        onPress={handleDeleteBudget}
+      >
+        <View className="mx-auto">
+          <Iconify
+            icon="material-symbols:delete-outline"
+            size={24}
+            color={CUSTOM_COLORS.accent}
+          />
+        </View>
+        <CustomText variant="accent">Delete</CustomText>
+      </TouchableOpacity>
+    </View>
+  )
+}
 
 const BudgetItem = ({ item }: { item: TBudget }) => {
   return (
-    <Link href={`/budgets/${item.id}`} asChild>
-      <TouchableOpacity className="bg-backgroundDimmed3 p-6 rounded-xl">
-        <CustomText>{item.name}</CustomText>
-      </TouchableOpacity>
-    </Link>
+    <Swipeable
+      renderRightActions={() => {
+        return <RightActions item={item} />
+      }}
+    >
+      <Link href={`/budgets/${item.id}`} asChild>
+        <TouchableOpacity className="bg-backgroundDimmed3 p-6 rounded-xl">
+          <CustomText>{item.name}</CustomText>
+        </TouchableOpacity>
+      </Link>
+    </Swipeable>
   )
 }
 
