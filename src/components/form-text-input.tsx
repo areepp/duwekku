17430dcd0d2322b270a17bc1480d6d508839f009
Arif Amijoyo <arifamjy@gmail.com>
2024-07-01 21:09:06 +0700

    feat: bind form text input with react hook form

--- src/components/form-text-input.tsx
+++ src/components/form-text-input.tsx
@@ -3,21 +3,31 @@ import { forwardRef } from 'react'
 import { TextInput, TextInputProps, View } from 'react-native'
 import colors from 'tailwindcss/colors'
 import CustomText from './custom-text'
+import { Controller, useFormContext } from 'react-hook-form'
 
-type FormTextInputProps = TextInputProps & {
+type CustomTextInputProps = TextInputProps & {
   customClassName?: string
   label?: string
 }
 
-const FormTextInput = forwardRef<TextInput, FormTextInputProps>(
+export const CustomTextInput = forwardRef<TextInput, CustomTextInputProps>(
   (
-    { onChangeText, value, placeholder, customClassName, label, keyboardType },
+    {
+      onChangeText,
+      value,
+      placeholder,
+      customClassName,
+      label,
+      keyboardType,
+      onFocus,
+      onBlur,
+    },
     ref,
   ) => {
     return (
       <View
         className={clsx(
-          'border-b py-3 border-accent text-text flex flex-row',
+          'border-b py-3 border-accent text-text flex flex-row w-full',
           customClassName,
         )}
         style={{
@@ -34,11 +44,36 @@ const FormTextInput = forwardRef<TextInput, FormTextInputProps>(
           placeholder={placeholder}
           placeholderTextColor={colors.zinc[700]}
           keyboardType={keyboardType}
+          onFocus={onFocus}
+          onBlur={onBlur}
           className="text-text flex-1"
         />
       </View>
     )
   },
 )
+
+type FormTextInputProps = CustomTextInputProps & {
+  name: string
+}
+
+const FormTextInput = ({ name, ...props }: FormTextInputProps) => {
+  const { control } = useFormContext()
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field: { onChange, value } }) => (
+        <CustomTextInput
+          {...props}
+          value={value}
+          onChangeText={(value) => {
+            onChange(value)
+          }}
+        />
+      )}
+    />
+  )
+}
 
 export default FormTextInput
