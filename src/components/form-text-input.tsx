17bf1b72f43a0c4419368ddc26bed55d055b6df9
Arif Amijoyo <arifamjy@gmail.com>
2024-07-13 11:45:11 +0700

    fix: showSoftInputOnFocus not working properly

--- src/components/form-text-input.tsx
+++ src/components/form-text-input.tsx
@@ -1,6 +1,12 @@
 import clsx from 'clsx'
-import { forwardRef } from 'react'
-import { TextInput, TextInputProps, View } from 'react-native'
+import { forwardRef, useEffect, useRef, useState } from 'react'
+import {
+  TextInput,
+  TextInputProps,
+  View,
+  Keyboard,
+  Platform,
+} from 'react-native'
 import colors from 'tailwindcss/colors'
 import CustomText from './custom-text'
 import { Controller, useFormContext } from 'react-hook-form'
@@ -8,6 +14,7 @@ import { Controller, useFormContext } from 'react-hook-form'
 type CustomTextInputProps = TextInputProps & {
   customClassName?: string
   label?: string
+  icon?: JSX.Element
 }
 
 export const CustomTextInput = forwardRef<TextInput, CustomTextInputProps>(
@@ -21,32 +28,64 @@ export const CustomTextInput = forwardRef<TextInput, CustomTextInputProps>(
       keyboardType,
       onFocus,
       onBlur,
+      editable,
+      showSoftInputOnFocus = true,
+      icon,
     },
     ref,
   ) => {
+    const [isKeyboardOpen, setIsKeyboardOpen] = useState(false)
+    const _ref = useRef<TextInput | null>(null)
+    const inputRef = ref ?? _ref
+
+    useEffect(() => {
+      const showListener = Keyboard.addListener('keyboardDidShow', () =>
+        setIsKeyboardOpen(true),
+      )
+      const hideListener = Keyboard.addListener('keyboardDidHide', () =>
+        setIsKeyboardOpen(false),
+      )
+      return () => {
+        showListener.remove()
+        hideListener.remove()
+      }
+    }, [])
+
     return (
       <View
         className={clsx(
-          'border-b py-3 border-accent text-text flex flex-row w-full',
+          'border-b py-3 border-accent text-text flex flex-row items-center w-full',
           customClassName,
         )}
         style={{
-          gap: 24,
+          gap: icon ? 4 : 24,
         }}
       >
         {label && (
           <CustomText customClassName="translate-y-1">{label}</CustomText>
         )}
+        {icon}
         <TextInput
-          ref={ref}
+          className="text-text flex-1"
+          ref={inputRef}
           onChangeText={onChangeText}
           value={value}
           placeholder={placeholder}
           placeholderTextColor={colors.zinc[700]}
           keyboardType={keyboardType}
-          onFocus={onFocus}
+          onFocus={(event) => {
+            const isAndroid = Platform.OS === 'android'
+            if (isAndroid && !showSoftInputOnFocus && isKeyboardOpen) {
+              Keyboard.dismiss()
+              // @ts-expect-error: current is not defined in type
+              inputRef.current?.focus()
+            } else {
+              onFocus?.(event)
+            }
+          }}
           onBlur={onBlur}
-          className="text-text flex-1"
+          editable={editable}
+          showSoftInputOnFocus={showSoftInputOnFocus}
         />
       </View>
     )
